a55e7e337ace28a81f6965cfdc423c51a86a4b32
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-05-03 22:54:14 +0200

    Very basic auto clicker that clicks every second

--- src/main/java/be/tristan/Main.java
+++ src/main/java/be/tristan/Main.java
@@ -1,7 +1,18 @@
 package be.tristan;
 
+import java.awt.*;
+import java.awt.event.InputEvent;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("test");
+    public static void main(String[] args) throws AWTException, InterruptedException {
+        final int clickDelay = 1000;
+        Robot r = new Robot();
+
+        while (true) {
+            r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+            r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+            System.out.println("Mouse button clicked");
+            Thread.sleep(clickDelay);
+        }
     }
 }
